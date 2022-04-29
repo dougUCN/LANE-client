8a8317db79a2818a8edc1645a32e2b8cf64027ae
Kevin Yeh <kevin.yay@gmail.com>
2022-04-28 17:32:19 -0700

    Convert index.js to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql/",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>
 );
 
@@ -17,3 +42,14 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Test query to make sure connection with graphql endpoint works
+client
+  .query({
+    query: gql`
+      query {
+        listHistograms
+      }
+    `,
+  })
+  .then((result) => console.log(result));
