10bb19a1369ed585c082a16a7adaf6190e88f18a
Kevin Yeh <kevin.yay@gmail.com>
2022-05-16 01:42:03 -0700

    Revert "Add craco to allow importing files not in src directory"
    This reverts commit 60d72fa23ac4e6f7cd0bc4aeb0e8350900333a36.

--- craco.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const disableModuleScopePlugin = require("./craco-disable-module-scope-plugin.js");
-
-module.exports = {
-  plugins: [{ plugin: disableModuleScopePlugin }],
-};
