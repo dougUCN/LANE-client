2cc8163814ef804756027e7987f729c90b6b3e5d
dougUCN <douglas.kt.wong@gmail.com>
2022-05-25 14:54:15 -0500

    Remove console log test and change action branch to main

--- src/index.tsx
+++ src/index.tsx
@@ -63,5 +63,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-console.log("This should stop a PR");
