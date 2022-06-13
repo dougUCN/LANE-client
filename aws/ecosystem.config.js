1f4d3acb40aeb208d0036cba16b604348c0f6a9c
dougUCN <douglas.kt.wong@gmail.com>
2022-06-13 01:18:23 -0600

    Clean up ecosystem file for PM2

--- aws/ecosystem.config.js
+++ aws/ecosystem.config.js
@@ -7,8 +7,6 @@ module.exports = {
       watch: "dist",
       env: {
         NODE_ENV: "development",
-        GRAPHQL_HTTP_ENDPOINT: "https://lane-server.herokuapp.com/graphql/",
-        GRAPHQL_WS_ENDPOINT: "ws://lane-server.herokuapp.com/graphql/",
       },
       log_date_format: "YYYY-MM-DD HH:mm Z",
     },
