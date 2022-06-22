40e03d861cb93a0b3a1d76b7f2710cf6953a7839
Kevin Yeh <kevin.yay@gmail.com>
2022-06-22 01:03:56 -0700

    Add query to get runConfigs

--- src/components/RunConfig/RunConfig.tsx
+++ src/components/RunConfig/RunConfig.tsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { useQuery } from "urql";
+import { GetRunConfigsDocument } from "generated";
+import LoadingSpinner from "components/shared/LoadingSpinner";
 
 const RunConfig = () => {
+  React.useEffect(() => {
+    document.title = "LANE - Run Config";
+  }, []);
+
+  const [result] = useQuery({
+    query: GetRunConfigsDocument,
+  });
+
+  const runConfigs = result?.data?.getRunConfigs?.runConfigs ?? [];
+
+  console.log("run configs", runConfigs);
+
+  if (result.fetching) {
+    return <LoadingSpinner className="mt-24" />;
+  }
+
   return <div className="dark:text-slate-100">This is the Run Config Page</div>;
 };
 
