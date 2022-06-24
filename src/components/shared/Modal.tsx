41c5e1c72311b81e5ad04ab7d873f5f29f2e0b19
Kevin Yeh <kevin.yay@gmail.com>
2022-06-24 01:06:53 -0700

    Use secondary button color for cancel button

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -40,34 +40,9 @@ const Modal = ({ isOpen, onClose }: Props) => {
             {/** Modal Footer */}
             <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
               <Button type="button">Create</Button>
-              <button
-                type="button"
-                className={clsx(
-                  "text-gray-500",
-                  "bg-white",
-                  "hover:bg-gray-100",
-                  "focus:ring-4",
-                  "focus:outline-none",
-                  "focus:ring-blue-300",
-                  "rounded",
-                  "border",
-                  "border-gray-200",
-                  "text-sm",
-                  "font-medium",
-                  "px-4",
-                  "py-2",
-                  "hover:text-gray-900",
-                  "focus:z-10",
-                  "dark:bg-gray-700",
-                  "dark:text-gray-300",
-                  "dark:border-gray-500",
-                  "dark:hover:text-white",
-                  "dark:hover:bg-gray-600",
-                  "dark:focus:ring-gray-600",
-                )}
-              >
+              <Button type="button" color="secondary">
                 Cancel
-              </button>
+              </Button>
             </div>
           </div>
         </div>
